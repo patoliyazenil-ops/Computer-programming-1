bd2d2b6a8280d7a9fb980260a3ed1ef60dccd55d
patoliyazenil-ops <patoliyazenil@gmail.com>
2025-08-16 15:52:49 +0530

    Create sum_n.c

---
+++ 02/03/04/05/06/07/08/09/sum_n.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int n, sum = 0, i;
+    printf("Enter N: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++)
+        sum += i;
+    printf("Sum = %d\n", sum);
+    return 0;
+}
